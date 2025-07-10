ffa975b0bb3699d3a65b4968451ee6bfed335d46
Marc Busqué <marc.busque@remote.com>
2025-07-10 15:23:45 +0200

    Use actual terminal!!

--- lua/ergoterm/pickers/p_fzf_lua.lua
+++ lua/ergoterm/pickers/p_fzf_lua.lua
@@ -23,43 +23,24 @@ function M.previewer:parse_entry(entry_str)
   if term then
     local bufnr = term:get_state("bufnr")
     local name = term.name
+    local layout = term:get_state("layout")
 
     return {
       bufnr = tonumber(bufnr),
-      name = name
+      name = name,
+      layout = layout,
+      do_not_cache = true
     }
   end
 end
 
---function M.previewer:populate_preview_buf(entry_str)
---  if not self.win or not self.win:validate_preview() then return end
---  local entry = self:parse_entry(entry_str)
---  self:set_preview_buf(entry.bufnr)
---  self.win:update_preview_title(" " .. entry.name .. " ")
---  self.win:update_preview_scrollbar()
---end
-
---function M.previewer:clear_preview_buf()
---  -- no-op
---end
+function M.previewer:safe_buf_delete()
+end
 
 function M.previewer:populate_preview_buf(entry_str)
-  if not self.win or not self.win:validate_preview() then return end
   local entry = self:parse_entry(entry_str)
-  if not entry then return end
-
-  -- Get a temporary buffer for the preview
-  local tmpbuf = self:get_tmp_buffer()
-
-  -- Copy the terminal buffer content to the preview buffer
-  local term_lines = vim.api.nvim_buf_get_lines(entry.bufnr, 0, -1, false)
-  vim.api.nvim_buf_set_lines(tmpbuf, 0, -1, false, term_lines)
-
-  -- Set the preview buffer
-  self:set_preview_buf(tmpbuf)
-
-  self.win:update_preview_title(" " .. entry.name .. " ")
-  self.win:update_preview_scrollbar()
+  self:set_preview_buf(entry.bufnr)
+  self.win:update_preview_title(" " .. entry.name .. " (" .. entry.layout .. ") ")
 end
 
 function M.previewer:gen_winopts()
