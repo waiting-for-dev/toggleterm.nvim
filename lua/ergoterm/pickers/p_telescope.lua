dca604387eabfd4bec6fb23599cec88c4a6fb40d
Marc Busqué <marc.busque@remote.com>
2025-07-12 18:05:09 +0200

    refactor: remove empty teardown functions in telescope picker

--- lua/ergoterm/pickers/p_telescope.lua
+++ lua/ergoterm/pickers/p_telescope.lua
@@ -29,8 +29,6 @@ function M.select(terminals, prompt, definitions)
         vim.api.nvim_win_set_buf(self.state.winid, bufnr)
       end)
     end,
-    teardown = function()
-    end
   })
 
   -- Create the picker
@@ -46,8 +44,6 @@ function M.select(terminals, prompt, definitions)
         }
       end,
     }),
-    teardown = function()
-    end,
     sorter = conf.generic_sorter({}),
     previewer = terminal_previewer,
     attach_mappings = function(prompt_bufnr, map)
